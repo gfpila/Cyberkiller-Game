a243c927794fbc2ad425c9fe6f138647398318b8
gabriellourencao <131073630+gabriellourencao@users.noreply.github.com>
2025-06-30 01:37:16 -0300

    Adicionando opção de mute e unmute no jogo

--- scenes/main-menu/main_menu.gd
+++ scenes/main-menu/main_menu.gd
@@ -11,3 +11,10 @@ func _on_sair_pressed() -> void:
 	$Audio/botao.play()
 	await get_tree().create_timer(0.675).timeout
 	get_tree().quit()
+
+
+
+func _on_opcoes_pressed() -> void:
+	$Audio/botao.play()
+	await get_tree().create_timer(0.675).timeout
+	get_tree().change_scene_to_file("res://scenes/opcoes/opcoes.tscn")
---
+++ scenes/opcoes/SoundOptions.gd
@@ -0,0 +1,20 @@
+extends CanvasLayer
+
+@onready var mute_button = $ButtonMute
+@onready var unmute_button = $ButtonUnmute
+
+func _ready():
+	mute_button.pressed.connect(_on_mute_pressed)
+	unmute_button.pressed.connect(_on_unmute_pressed)
+
+func _on_mute_pressed():
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	mute_button.visible = false
+	unmute_button.visible = true
+	get_tree().change_scene_to_file("res://scenes/main-menu/main_menu.tscn")
+
+func _on_unmute_pressed():
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
+	mute_button.visible = true
+	unmute_button.visible = false
+	get_tree().change_scene_to_file("res://scenes/main-menu/main_menu.tscn")	
